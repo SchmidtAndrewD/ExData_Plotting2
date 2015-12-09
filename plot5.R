7bdaa52723a5d8c56d0281a666b3a85bf88e1bc1
Andrew Schmidt <Andrew.Schmidt2@gmail.com>
2015-12-09 13:54:52 -0500

    <Plot 5 Work>

---
+++ plot5.R
@@ -0,0 +1,46 @@
+##################################################################################################
+##################################################################################################
+##################################################################################################
+#########               Course Project 2  - Plot4.R                       ########################
+##################################################################################################
+##################################################################################################
+##################################################################################################
+
+
+##  Set Working Directory
+setwd("C:/Users/Andrew/Desktop/Data Science Course/Exploratory Data Analysis/Course Project 2")
+
+##  Read in files
+if(!exists("NEI")){
+  NEI <- readRDS("./data/summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("./data/Source_Classification_Code.rds")
+}
+# merge the two data sets 
+if(!exists("NEISCC")){
+  NEISCC <- merge(NEI, SCC, by="SCC")
+}
+
+
+library(ggplot2)
+
+# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
+
+# 24510 is Baltimore, see plot2.R
+# Searching for ON-ROAD type in NEI
+# Don't actually know it this is the intention, but searching for 'motor' in SCC only gave a subset (non-cars)
+NEI_subset2 <- NEI[NEI$fips=="24510" & NEI$type=="ON-ROAD",  ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI_subset2, sum)
+
+
+
+png("plot5.png", width=840, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions from Motor Vehicle (Type: ON-ROAD) in Baltimore City, Maryland (FIP: 24510) [1999 to 2008]')
+print(g)
+dev.off()
